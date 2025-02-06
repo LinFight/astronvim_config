f84fbfbcf76b755f165b0235afe143ad88f4dc2c
LinFight <1095722147@qq.com>
2025-02-06 15:46:32 +0800

    ad:pack of prisma typescript

--- lua/community.lua
+++ lua/community.lua
@@ -6,6 +6,8 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.prisma" },
   { import = "astrocommunity.pack.elixir-phoenix" },
   { import = "astrocommunity.pack.html-css" },
   -- { import = "astrocommunity.completion.blink-cmp" },
